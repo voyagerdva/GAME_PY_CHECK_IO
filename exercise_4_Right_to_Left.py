7b204b78177b2dcb10b75302110b78bda780bc9f
voyagerdva <mariner-9@yndex.ru>
2021-11-16 18:25:02 +0300

    4 and 5 exr

--- exercise_4_Right_to_Left.py
+++ exercise_4_Right_to_Left.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     print("Example:")
     print(left_join(("left", "right", "left", "stop")))
 
-    # These "asserts" using only for self-checking and not necessary for auto-testing
+# These "asserts" using only for self-checking and not necessary for auto-testing
 # assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop"    )
 assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
 assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
