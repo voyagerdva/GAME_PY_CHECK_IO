49e3a88ef00a129ca6ffc4f5cd5b2ccc6d8e3223
voyagerdva <mariner-9@yndex.ru>
2021-11-26 20:03:58 +0300

    8th excrs

---
+++ exercise_8_ TOP_PRICE.py
@@ -0,0 +1,14 @@
+def bigger_price(N, data):
+    pass
+
+
+data = [
+    {"name": "bread", "price": 100},
+    {"name": "wine", "price": 138},
+    {"name": "meat", "price": 15},
+    {"name": "water", "price": 1}
+]
+
+N = 2
+
+print(bigger_price(N, data))
