17eda85a065b142db7cf6e2a4f93c5f18a372d0e
voyagerdva <mariner-9@yndex.ru>
2021-11-27 15:02:42 +0300

    8 exrcs

--- exercise_8_ TOP_PRICE.py
+++ exercise_8_ TOP_PRICE.py
@@ -1,8 +1,24 @@
-def bigger_price(N, data):
-    pass
+def bigger_price(N, table):
+    resultValues = sorted([list(good.values())[1] for good in table], reverse=True)[:N]
+    result = [item for item in table if list(item.values())[1] in resultValues]
 
+    result.sort(key= lambda item: list(item.values())[1], reverse=True)
 
-data = [
+    return result
+    # result = sorted(table, reverse=True, key=table)
+    #
+    # result = []
+    # for item in table:
+    #     # table.sort(reverse=True, key=item["price"])
+    #     print(item["price"])
+    #
+    # # result = [table.sort(reverse=True, key=item["price"]) for item in table]
+    # # result =
+    # # pass
+    # # return
+    # # return table
+
+table = [
     {"name": "bread", "price": 100},
     {"name": "wine", "price": 138},
     {"name": "meat", "price": 15},
@@ -11,4 +27,4 @@ data = [
 
 N = 2
 
-print(bigger_price(N, data))
+print(bigger_price(N, table))
