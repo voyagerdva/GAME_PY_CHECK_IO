fdf6dabc31556b5e998f4f351f7b6e08550ed0ee
voyagerdva <mariner-9@yndex.ru>
2021-12-03 21:08:59 +0300

    10th excrs

--- exercise_10_Non-unique_elements.py
+++ exercise_10_Non-unique_elements.py
@@ -1,12 +1,34 @@
+def lessList(data, i):
+    return data[:i] + data[i + 1:]
+
+
 def checkio(data: list) -> list:
-    dataUnique = []
+    i = 0
+
+    if len(set(data)) == len(data):
+        return []
+
+    newData = []
+
     for item in data:
-        if item not in dataUnique:
-            dataUnique.append(item)
+        if item in data[:i] + data[i + 1:]:
+            i += 1
+            newData.append(item)
+            continue
+        else:
+            i += 1
+            continue
+    return newData
 
-    return dataUnique
 
-
-data = [1, 2, 3, 1, 3]
+data = [10,20,30,10]
 
 print(checkio(data))
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
