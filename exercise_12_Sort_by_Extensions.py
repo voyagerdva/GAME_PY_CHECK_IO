067494c91f30b208341a9785d90d864136ccb0c0
voyagerdva <mariner-9@yndex.ru>
2021-12-09 21:37:10 +0300

    some commit...

---
+++ exercise_12_Sort_by_Extensions.py
@@ -0,0 +1,26 @@
+from typing import List
+def sort_by_ext(files: List[str]) -> List[str]:
+    pass
+
+
+# def frequency_sort(arr):
+#     elements = {}
+#     for item in arr:
+#         if not elements.get(item):
+#             elements[item] = 1
+#         else:
+#             elements[item] += 1
+#
+#     result = []
+#     for k in elements.keys():
+#         for i in range(elements[k]):
+#             result.append(k)
+#
+#     return result
+#
+#
+#
+#
+# arr = [4, 6, 2, 2, 2, 6, 4, 4, 4]
+#
+# print(frequency_sort(arr))
